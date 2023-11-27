afa98e12132ed9244ecae1b9c0f6a62b13fe0148
sebastian461 <sebastiano-461@hotmail.com>
2023-11-26 23:26:31 -0500

    Añadiendo estilos globales

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
 /* Esta es la sintaxis a seguir para renderizar los componentes en el index.html a traves del modo estricto */
 ReactDOM.createRoot(document.getElementById("root")).render(
