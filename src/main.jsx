d8f03555d092f11f65e7e17328e9b45f02e3bef9
sebastian461 <sebastiano-461@hotmail.com>
2023-11-26 22:22:50 -0500

    Hola mundo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
 
+/* El punto de entrada de una apliación React es a traves de un functional component, así: 
+(todos los functional componenten deben ser en PascalCase)*/
+function App(){
+  // El retorn es equivalente a un document.createElement...
+  return <h1>Hola mundo!!!</h1>;
+}
+
+/* Esta es la sintaxis a seguir para renderizar los componentes en el index.html a traves del modo estricto */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
